1180bd07bf93495c393a2c26af8b88d455cf378a
Aleksandr Kanunnikov <lifeart92@gmail.com>
2019-12-04 03:34:48 +0300

    basic implementation

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import fetch from 'node-fetch';
 import { isComponent, getComponentName, isComponentArgument } from './lib/ast-helpers';
 import { normalizeComponents } from './lib/normalizers';
+import { URI } from 'vscode-uri';
 
 async function getAddonDocs(org, repo) {
     const result = await fetch(`https://${org}.github.io/${repo}/docs/${repo}.json`);
@@ -8,16 +9,38 @@ async function getAddonDocs(org, repo) {
     return data;
 }
 
-async function loadCompletions() {
-    const data = await getAddonDocs('miguelcobain', 'ember-yeti-table');
-    return normalizeComponents(data);
+async function loadCompletions(addons) {
+    /* 
+        this implementation just draft,
+        we should cache it
+    */
+    const data = await Promise.all(addons.map(([org, repo])=> await getAddonDocs(org, repo)));
+    const addons = data.map((info)=>normalizeComponents(info));
+    const results = [].concat(data);
+    return Object.assign.apply({}, results);
 }
 
-async function onComplete(_,{ results, focusPath, type }) {
+async function findProjectAddonsWithDocs(projectRoot) {
+    /* 
+        we need to get package.json, extract all names, 
+        find related folders, check for ember-addon key in package.json
+        check if addon-docs exists for such project
+        if exists - return github org name and repo name (for now)
+
+        I beleve we can cache it for projectRoot
+    */
+    return [
+        ['miguelcobain', 'ember-yeti-table']
+    ];
+}
+
+async function onComplete(root,{ results, focusPath, type }) {
     if (type !== 'template') {
         return results;
     }
-    const components = await loadCompletions();
+    const projectRoot = URI.parse(root).fsPath;
+    const addons = await findProjectAddonsWithDocs(projectRoot);
+    const components = await loadCompletions(addons);
     if (isComponent(focusPath)) {
         Object.keys(components).forEach((name) => {
             if (!results.filter(({label})=>label === name)) {
