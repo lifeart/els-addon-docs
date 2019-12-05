aef07ff39ed13686a573c94a6a567bd104821e8e
Aleksandr Kanunnikov <lifeart92@gmail.com>
2019-12-05 22:19:10 +0300

    code cleanup

--- index.js
+++ index.js
@@ -1,74 +1,21 @@
-const fetch = require("node-fetch");
 const {
   isComponent,
   getComponentName,
   isComponentArgument,
+  is,
   isScopedPathExpression
 } = require("./lib/ast-helpers");
 const memoize = require("memoizee");
-const { normalizeComponents } = require("./lib/normalizers");
+const { mergeLeft } = require("./lib/normalizers");
 const { URI } = require("vscode-uri");
 const { getProjectAddonsInfo } = require("./lib/dependency-collector");
-const PLACEHOLDER = 'ELSCompletionDummy';
-
-async function getAddonDocs(packageName, demoURL) {
-  const result = await fetch(`${demoURL}/docs/${packageName}.json`);
-  const data = await result.json();
-  return data;
-}
-
-async function loadCompletions(addons) {
-  /* 
-        this implementation just draft,
-        we should cache it
-    */
-  const mapper = async ([org, repo]) => {
-    let result = {
-      included: []
-    };
-    try {
-      result = await getAddonDocs(org, repo);
-    } catch (e) {
-      console.error(e.stack);
-    }
-    return result;
-  };
-  const queries = addons.map(mapper);
-  const data = await Promise.all(queries);
-  const addonsResults = data.map(info => normalizeComponents(info));
-  const results = [].concat(addonsResults);
-  return Object.assign.apply({}, results);
-}
-
-function demoUrl(pack) {
-  return pack["ember-addon"].demoURL;
-}
-function addonName(pack) {
-  return pack["name"];
-}
-
-function hasDemoUrl(pack) {
-  return (
-    pack["ember-addon"] &&
-    typeof pack["ember-addon"].demoUrl === "string" &&
-    pack["ember-addon"].demoUrl.length
-  );
-}
+const { makeAddonUrl, loadCompletions } = require("./lib/doc-resolver");
+const PLACEHOLDER = "ELSCompletionDummy";
 
 async function findProjectAddonsWithDocs(projectRoot) {
   const items = getProjectAddonsInfo(projectRoot);
 
-  return items
-    .map(el => {
-      if (hasDemoUrl(el)) {
-        return [addonName(el), demoUrl(el)];
-      } else if (el.homepage) {
-        return [addonName(el), el.homepage];
-      } else {
-        return null;
-      }
-    })
-    .filter(el => el !== null);
+  return items.map(makeAddonUrl).filter(el => el !== null);
   /* 
         we need to get package.json, extract all names, 
         find related folders, check for ember-addon key in package.json
@@ -94,14 +41,6 @@ async function findProjectAddonsWithDocs(projectRoot) {
   ];
 }
 
-function mergeLeft(to, from) {
-  Object.keys(from).forEach(key => {
-    if (!to[key]) {
-      to[key] = from[key];
-    }
-  });
-}
-
 async function loadAddonInfo(projectRoot) {
   const addons = await findProjectAddonsWithDocs(projectRoot);
   const components = await loadCompletions(addons);
@@ -110,20 +49,46 @@ async function loadAddonInfo(projectRoot) {
 
 const addonInfo = memoize(loadAddonInfo);
 
+function byLabel(results, name) {
+  return results.find(({ label }) => label === name);
+}
+
+function normalizeArgumentName(name) {
+  return '@' + name.replace('@', '');
+}
+
+function pathRoot(name) {
+  return name.split('.')[0];
+}
+
+function replaceRoot(path, root) {
+  const parts = path.split('.');
+  parts[0] = root;
+  return parts.join('.');
+}
+
+function onlyValidBlockNames(results) {
+  return results.filter(({ label }) => /^[A-z0-9]+$/.test(label));
+}
+
+function isComponentCompletion({kind}) {
+  return kind === 7;
+}
+
 async function onComplete(root, { results, focusPath, type }) {
   if (type !== "template") {
     return results;
   }
   const projectRoot = URI.parse(root).fsPath;
   const components = await addonInfo(projectRoot);
-  const meta = focusPath.metaForType('handlebars');
+  const meta = focusPath.metaForType("handlebars");
   if (isComponentArgument(focusPath)) {
     const componentName = getComponentName(focusPath);
     if (componentName in components) {
       components[componentName].arguments.forEach(arg => {
-        const argument = results.find(({ label }) => label === arg.name);
+        const argument = byLabel(results, arg.name);
         const argumentInfo = {
-          label: "@" + arg.name,
+          label: normalizeArgumentName(arg.name),
           kind: 5,
           detail: arg.description,
           documentation: arg.description
@@ -138,15 +103,15 @@ async function onComplete(root, { results, focusPath, type }) {
   } else {
     const blockDefinition = meta.maybeBlockParamDefinition;
     if (blockDefinition) {
-      if (blockDefinition.path.node.type === 'ElementNode') {
+      if (is(blockDefinition.path, "ElementNode")) {
         const componentName = blockDefinition.path.node.tag;
         let localResult = [];
         if (componentName in components) {
-          components[componentName].yields.forEach((el)=>{
-            if (!el.label.includes('.') && blockDefinition.index === el.index) {
+          components[componentName].yields.forEach(el => {
+            if (!el.label.includes(".") && blockDefinition.index === el.index) {
               localResult.push(el);
             }
-          })
+          });
         }
         if (localResult.length) {
           results = localResult;
@@ -154,34 +119,35 @@ async function onComplete(root, { results, focusPath, type }) {
             results[0].preselect = true;
           }
         } else {
-          results = results.filter(({label})=>/^[A-z0-9]+$/.test(label));
+          results = onlyValidBlockNames(results);
         }
       }
     } else if (isScopedPathExpression(focusPath)) {
-      meta.localScope.forEach((scopeItem)=>{
-        const originalPathPrefix = focusPath.node.original.replace(PLACEHOLDER, '') || scopeItem.name;
-        const shouldComplete = scopeItem.name.startsWith(originalPathPrefix.split('.')[0]);
-        if (shouldComplete && scopeItem.path.node.type === 'ElementNode') {
+      meta.localScope.forEach(scopeItem => {
+        const originalPathPrefix =
+          focusPath.node.original.replace(PLACEHOLDER, "") || scopeItem.name;
+        const shouldComplete = scopeItem.name.startsWith(
+          pathRoot(originalPathPrefix)
+        );
+        if (shouldComplete && is(scopeItem.path, "ElementNode")) {
           const componentName = scopeItem.path.node.tag;
           if (componentName in components) {
-            components[componentName].yields.forEach((el)=>{
-              let fixedName = el.label.split('.');
-              fixedName[0] = scopeItem.name;
-              if (el.kind !== 7) {
+            components[componentName].yields.forEach(el => {
+              if (!isComponentCompletion(el)) {
                 results.push({
-                  label: fixedName.join('.'),
+                  label: replaceRoot(el.label, scopeItem.name),
                   kind: el.kind,
                   detail: el.detail
                 });
               }
-            })
+            });
           }
         }
-      })
-      /// 
+      });
+      ///
     } else if (isComponent(focusPath)) {
       Object.keys(components).forEach(name => {
-        const component = results.find(({ label }) => label === name);
+        const component = byLabel(results, name)
         const info = {
           label: name,
           detail: components[name].description,
@@ -195,28 +161,29 @@ async function onComplete(root, { results, focusPath, type }) {
         }
       });
 
-      meta.localScope.forEach((scopeItem)=>{
-        if (scopeItem.path.node.type === 'ElementNode') {
+      meta.localScope.forEach(scopeItem => {
+        if (is(scopeItem.path, "ElementNode")) {
           const componentName = scopeItem.path.node.tag;
-          const originalPathPrefix = focusPath.node.tag.replace(PLACEHOLDER, '') || scopeItem.name;
-          const shouldComplete = scopeItem.name.startsWith(originalPathPrefix.split('.')[0]);
+          const originalPathPrefix =
+            focusPath.node.tag.replace(PLACEHOLDER, "") || scopeItem.name;
+          const shouldComplete = scopeItem.name.startsWith(
+            pathRoot(originalPathPrefix)
+          );
           if (shouldComplete && componentName in components) {
-            components[componentName].yields.forEach((el)=>{
-              if (el.kind === 7) {
-                let fixedName = el.label.split('.');
-                fixedName[0] = scopeItem.name;
+            components[componentName].yields.forEach(el => {
+              if (isComponentCompletion(el)) {
                 results.push({
-                  label: fixedName.join('.'),
+                  label: replaceRoot(el.label, scopeItem.name),
                   kind: el.kind,
                   description: el.description,
                   detail: el.detail
                 });
               }
-            })
+            });
           }
         }
-      })
-    } 
+      });
+    }
   }
   return results;
 }
